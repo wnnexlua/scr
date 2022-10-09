04924d34c88464cd57022521500524cee1ae34a4
wnnex <96862897+wnnexlua@users.noreply.github.com>
2022-10-08 19:00:27 -0700

    Universal Chat Spy

--- chatspy.lua
+++ chatspy.lua
@@ -58,3 +58,5 @@ if not player.PlayerGui:FindFirstChild("Chat") then wait(3) end
 local chatFrame = player.PlayerGui.Chat.Frame
 chatFrame.ChatChannelParentFrame.Visible = true
 chatFrame.ChatBarParentFrame.Position = chatFrame.ChatChannelParentFrame.Position+UDim2.new(UDim.new(),chatFrame.ChatChannelParentFrame.Size.Y)
+
+-- W/ WNNEX
